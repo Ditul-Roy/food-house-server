d9758f448996da0e28f1cb61750b8f163474cd48
Ditul-Roy <ditulr@gmail.com>
2023-05-02 09:01:29 +0600

    recipes data added

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const app = express();
 const cors = require('cors');
 const port = 5000;
 
-const chef = require('./data/chefData.json')
+const chef = require('./data/chefData.json');
+const recipes = require('./data/recipes.json')
 
 app.use(cors());
+
 app.get('/', (req, res) =>{
     res.send('hello from server')
 });
@@ -17,6 +19,15 @@ app.get('/chef/:id', (req,res) =>{
    const id = req.params.id;
    const chefId = chef.find(c=> c._id === id);
    res.send(chefId)
+})
+
+app.get('/recipes', (req, res) => {
+    res.send(recipes)
+})
+app.get('/recipes/:id',(req,res) =>{
+    const id = req.params.id;
+    const recipe = recipes.filter(r=>r.recipe_id===id);
+    res.send(recipe)
 })
 
 app.listen(port, () => {
